fc3f674023e6ab05c052327aed1ddf2da1a63381
Rajput Shivam Chandan <108003304+Rshivam026@users.noreply.github.com>
2022-12-27 22:24:01 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -3,22 +3,23 @@ let API_KEY = "04e45c30721b3ba81b9d381ad8dca978";
 let xhr = new XMLHttpRequest();
 // 04e45c30721b3ba81b9d381ad8dca978
 // "https://newsapi.org/v2/top-headlines?country=in&apiKey=5a8ab37aa4a344b09794784a4b4b3868"
-xhr.open('GET' ,'https://gnews.io/api/v4/search?q=example&token=' + API_KEY + '&lang=en&country=in&max=10' ,true);
+xhr.open('GET' ,'https://newsdata.io/api/1/news?apikey=pub_14741e6e81001deef2e4af7d9dcec91ba6d76&country=in,us&language=en,hi' ,true);
 xhr.send();
 
 
 xhr.onload = function(){
     let str = "";
     let data = JSON.parse(this.responseText);
-    let result = data["articles"];
+    console.log(data);
+    let result = data["results"];
     let index = 0;
     for(element of result)
       {
-        str += `
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-heading${index}">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${index}" aria-expanded="true" aria-controls="panelsStayOpen-collapse${index}">
-              <strong>Breaking News -- ${index+1} ---</strong>${element["title"]}
+        str += `<hr id="line">
+        <div class="accordion-item ">
+          <h2 class="accordion-header " id="panelsStayOpen-heading${index}">
+            <button class="accordion-button newslist" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${index}" aria-expanded="true" aria-controls="panelsStayOpen-collapse${index}" style = "color:black; border:2px solid black; font-size:1.1rem;">
+              <strong>Breaking News --  ${index+1} --  </strong>${element["title"]}
             </button>
           </h2>
           <div id="panelsStayOpen-collapse${index}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading${index}">
